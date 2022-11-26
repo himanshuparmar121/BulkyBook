94eb07da2f7cb545c144b078b8ffb970bae480c0
himanshuparmar121 <parmarhimanshu360@gmail.com>
2022-11-26 17:02:47 +0530

    Added model, updated database and added repository for Cover Type.

---
+++ BulkyBook.Data.Access/Repository/CoverTypeRepository.cs
@@ -0,0 +1,26 @@
+﻿using BulkyBook.Data.Access.Repository.IRepository;
+using BulkyBook.DataAccess;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Data.Access.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        readonly private ApplicationDbContext _db;
+
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(CoverType obj)
+        {
+            _db.CoverTypes.Update(obj);
+        }
+    }
+}
